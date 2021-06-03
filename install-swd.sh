3069ae563bbacf00cb127b382b41998d0d642965
Marcelo Bezerra <mmosca@github.com>
2021-06-03 12:38:12 +0200

    Add script to program pico with openocd

---
+++ install-swd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+OPENOCD_IFACE=/usr/share/openocd/scripts/interface/raspberrypi-swd.cfg
+OPENOCD_TARGET=/usr/share/openocd/scripts/target/rp2040.cfg
+
+set -x
+
+openocd -f $OPENOCD_IFACE -f $OPENOCD_TARGET -c "program pico-sbus.elf verify reset exit"
