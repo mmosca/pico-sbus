0a370a28166c4efc28744b394fcd706a6b1ab4b6
Marcelo Bezerra <mmosca@github.com>
2021-06-05 13:30:42 +0200

    Add sbus -> joy translation

--- sbus-hid.c
+++ sbus-hid.c
@@ -1,4 +1,4 @@
-#include "sbus-hid.h"
+#include <string.h>
 
 #include "tusb.h"
 #include "pico/stdlib.h"
@@ -6,7 +6,69 @@
 
 #include "bsp/board.h"
 
+#include "sbus-hid.h"
 #include "sbus.h"
+
+struct axis_button_mapping_t {
+   int channel;
+   int first_button; 
+} axis_button_mapping[] = {
+    {.channel = 4, .first_button = 0},
+    {6, 2},
+    {8, 5},
+    {9, 8},
+    {10, 11},
+    {11, 14},
+    {12, 17},
+    {13, 20},
+    {14, 23},
+    {15, 26},
+    {-1, -1}};
+
+joystick_button_t channel2button(uint16_t channelValue)
+{
+    if(channelValue < SBUS_HID_LOW_TH)
+    {
+        return BUTTON_LOW;
+    } else if(channelValue > SBUS_HID_HIGH_TH)
+    {
+        return BUTTON_HIGH;
+    }
+
+    return BUTTON_MID;
+}
+
+void setJoyStickButton(joystick_state_t *joystick, int low_button, uint16_t channel_value)
+{
+    int b = channel2button(channel_value);
+    joystick->buttons[low_button + b] = true;
+}
+
+void sbus2joystick(const sbus_state_t *sbus, joystick_state_t *joystick)
+{
+    memset(joystick, 0, sizeof(joystick_state_t));
+
+    joystick->yr = sbus->ch[0];
+    joystick->xr = sbus->ch[1];
+    joystick->yl = sbus->ch[2];
+    joystick->xl = sbus->ch[3];
+    joystick->z = sbus->ch[5];
+    joystick->z_rot = sbus->ch[7];
+
+    for (int i = 0; axis_button_mapping[i].channel != -1; ++i)
+    {
+        setJoyStickButton(joystick, axis_button_mapping[i].first_button, sbus->ch[axis_button_mapping[i].channel]);
+    }
+
+    if (sbus->ch[16] > SBUS_HID_HIGH_TH)
+    {
+        joystick->buttons[29] = true;
+    }
+    if (sbus->ch[17] > SBUS_HID_HIGH_TH)
+    {
+        joystick->buttons[30] = true;
+    }
+}
 
 void hid_init()
 {
@@ -20,6 +82,7 @@ void hid_init()
 void hid_main()
 {
     uint8_t sbusData[SBUS_MESSAGE_MAX_SIZE] = {};
+    joystick_state_t joy = {};
     int clear_counter = 0;
 
     while(1)
@@ -45,7 +108,7 @@ void hid_main()
 
 
                 decode_sbus_data(sbusData, &sbus);
-
+                sbus2joystick(&sbus, &joy);
 
                 for(int i = 0; i < 18; ++i)
                 {
@@ -53,6 +116,15 @@ void hid_main()
                 }
                 printf("Frame lost: %i Failsafe: %i\n", sbus.framelost, sbus.failsafe);
                 gpio_put(PICO_DEFAULT_LED_PIN, 0);
+
+                printf("Joy L: %i, %i\n", joy.xl, joy.yl);
+                printf("Joy R: %i, %i\n", joy.xr, joy.yr);
+                printf("Joy Z: %i, %i\n", joy.z, joy.z_rot);
+
+                for (int i = 0; i < SBUS_HID_MAX_BUTTONS; ++i)
+                {
+                    printf("Button %2i: %i\n", i + 1, joy.buttons[i]);
+                }
             }
 
             sleep_ms(9);
--- sbus-hid.h
+++ sbus-hid.h
@@ -1,7 +1,38 @@
 #ifndef __SBUS_HID_H__
 #define __SBUS_HID_H__
 
+#include <stdbool.h>
+
+#define SBUS_HID_MAX_BUTTONS 31
+
+#define SBUS_HID_MIN 0
+#define SBUS_HID_MAX 0x7FF
+
+#define SBUS_HID_LOW_TH (SBUS_HID_MAX / 3)
+#define SBUS_HID_HIGH_TH (SBUS_HID_MAX - SBUS_HID_LOW_TH)
+
+typedef struct {
+    uint16_t xl;
+    uint16_t yl;
+
+    uint16_t xr;
+    uint16_t yr;
+
+    uint16_t z;
+    uint16_t z_rot;
+
+    bool buttons[SBUS_HID_MAX_BUTTONS];
+} joystick_state_t;
+
+typedef enum joystyick_button {
+    BUTTON_LOW = 0,
+    BUTTON_MID = 1,
+    BUTTON_HIGH = 3
+} joystick_button_t;
+
 void hid_init();
 void hid_main();
+
+joystick_button_t channel2button(uint16_t channelValue);
 
 #endif
