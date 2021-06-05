d7388393dfabbb6adaabd936abf1a80fca7c7d51
Marcelo Bezerra <mmosca@github.com>
2021-06-05 14:42:47 +0200

    Adapt to tinyusb example hid_composite

--- sbus-hid.c
+++ sbus-hid.c
@@ -87,18 +87,15 @@ void hid_main()
 
     while(1)
     {
-#ifdef USB_HID
         tud_task(); // device task
-        //tuh_task(); // host task
-#endif
-        if ((clear_counter++) % 80000)
-        {
-            printf("\033[2J");
-        }
+
 
         if(hasSbusData())
         {
+            /*
+            printf("\033[2J");
             printf("\033[H");
+             */
 
             if(readSbusData(sbusData))
             {
@@ -139,67 +136,20 @@ void hid_main()
     }
 }
 
-#ifdef USB_HID
-//--------------------------------------------------------------------+
-// USB HID
-//--------------------------------------------------------------------+
-
-void hid_task(void) {
-    // Poll every 10ms
-    const uint32_t interval_ms = 10;
-    static uint32_t start_ms = 0;
-
-    if (board_millis() - start_ms < interval_ms) return; // not enough time
-    start_ms += interval_ms;
-
-    uint32_t const btn = 1;
-
-    // Remote wakeup
-    if (tud_suspended() && btn) {
-        // Wake up host if we are in suspend mode
-        // and REMOTE_WAKEUP feature is enabled by host
-        tud_remote_wakeup();
-    }
-
-    /*------------- Mouse -------------*/
-    if (tud_hid_ready()) {
-        if (btn) {
-            int8_t const delta = 5;
-
-            // no button, right + down, no scroll pan
-            tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, delta, delta, 0, 0);
-
-            // delay a bit before attempt to send keyboard report
-            board_delay(10);
-        }
-    }
-
-    /*------------- Keyboard -------------*/
-    if (tud_hid_ready()) {
-        // use to avoid send multiple consecutive zero report for keyboard
-        static bool has_key = false;
-
-        static bool toggle = false;
-        if (toggle = !toggle) {
-            uint8_t keycode[6] = {0};
-            keycode[0] = HID_KEY_A;
-
-            tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
-
-            has_key = true;
-        } else {
-            // send empty key report if previously has key pressed
-            if (has_key) tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
-            has_key = false;
-        }
-    }
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize) {
+    // TODO set LED based on CAPLOCK, NUMLOCK etc...
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) bufsize;
 }
-
 
 // Invoked when received GET_REPORT control request
 // Application must fill buffer report's content and return its length.
 // Return zero will cause the stack to STALL request
-uint16_t tud_hid_get_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen) {
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen) {
     // TODO not Implemented
     (void) report_id;
     (void) report_type;
@@ -208,29 +158,3 @@ uint16_t tud_hid_get_report_cb(uint8_t report_id, hid_report_type_t report_type,
 
     return 0;
 }
-
-// Invoked when received SET_REPORT control request or
-// received data on OUT endpoint ( Report ID = 0, Type = 0 )
-void tud_hid_set_report_cb(uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize) {
-    // TODO set LED based on CAPLOCK, NUMLOCK etc...
-    (void) report_id;
-    (void) report_type;
-    (void) buffer;
-    (void) bufsize;
-}
-
-//--------------------------------------------------------------------+
-// BLINKING TASK
-//--------------------------------------------------------------------+
-void led_blinking_task(void) {
-    static uint32_t start_ms = 0;
-    static bool led_state = false;
-
-    // Blink every interval ms
-    if (board_millis() - start_ms < blink_interval_ms) return; // not enough time
-    start_ms += blink_interval_ms;
-
-    board_led_write(led_state);
-    led_state = 1 - led_state; // toggle
-}
-#endif
