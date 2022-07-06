e5d68e773f2a47d8fa4b45c47e83589a559d2f6f
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2022-07-06 18:33:50 +0100

    small changes to allow specifying a prefix for the build

--- install-swd.sh
+++ install-swd.sh
@@ -2,6 +2,17 @@
 OPENOCD_IFACE=interface/raspberrypi-swd.cfg
 OPENOCD_TARGET=target/rp2040.cfg
 
+PREFIX=$1
+
+
 set -x
 
-openocd -f $OPENOCD_IFACE -f $OPENOCD_TARGET -c "program pico-sbus.elf verify reset exit"
+if [ $PREFIX = "" ]
+then
+
+  openocd -f $OPENOCD_IFACE -f $OPENOCD_TARGET -c "program pico-sbus.elf verify reset exit"
+
+else
+
+  openocd -f $OPENOCD_IFACE -f $OPENOCD_TARGET -c "program $PREFIX/pico-sbus.elf verify reset exit"
+fi
